615fa5f8b048d48ac618a66772a8e6d1b126b971
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2022-12-27 19:24:58 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Deafault Jumping Speed")]
+    public float jumpPower = 6f;
+    [Header("Boolean isGrounded")]
+    public bool isGrounded = false;
+    float posX = 0.0f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * (jumpPower * rb.mass))
+        }
+    }
+}
