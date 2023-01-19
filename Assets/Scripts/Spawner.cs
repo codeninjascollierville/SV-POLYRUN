22ef162b481146d3bba305d05ef19b10183d61f0
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-01-18 19:06:52 -0600

    u fat
    u fat

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,16 +10,20 @@ public class Spawner : MonoBehaviour
     public float spawnDelay = 1f;
     [Header("Default spawn time")]
     public float spawnTime = 2f;
+    [Header("Deafault Speed")]
+    public float speed;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+        
         InvokeRepeating("InstintiateObjects", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(0.2f, -3.86f, 0);
+        transform.position = new Vector3(4.03f, -0.16f, 0);
+        transform.position += Vector3.left * speed * Time.deltaTime;
     }
 
     void InstintiateObjects()
